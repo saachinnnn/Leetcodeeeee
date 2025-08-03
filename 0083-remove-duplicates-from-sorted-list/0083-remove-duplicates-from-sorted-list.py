43c1aef214c3497f5aa89364bf190bc8f6741321
Sachin <saachinnnn7@gmail.com>
2025-08-03 14:24:19 +0530

    0083-REMOVE-DUPLICATES-FROM-SORTED-LIST Stats: Time: 34 ms (2.54%), Space: 17.8 MB (65.49%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.py
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.py
@@ -1,34 +1,22 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        ''' How to solve this problem....
-            Steps:-
-            1) Just use the dummy node method and keep a tailpointer and maintain a hashset LMFAO.
-            2) DONE TA DA!
-            '''
-
-            # First the edge cases.
-        if head is None:
-            return None
-        elif head.next is None:
+        if head is None or head.next is None:
             return head
-        else:
-            Hashset : set = set()
-            dummy = ListNode(-1)
-            tail = dummy
-            tail.next = head
-                
-            curr = head
-            while curr is not None:
-                if curr.val in Hashset:
-                    tail.next = curr.next
-                else:
-                    Hashset.add(curr.val)
-                    tail.next = curr
-                    tail = tail.next
-                curr = curr.next
-            return dummy.next
+
+        seen = set()
+        dummy = ListNode(-1)
+        tail = dummy
+        curr = head
+
+        while curr:
+            if curr.val not in seen:
+                seen.add(curr.val)
+                tail.next = curr
+                tail = tail.next
+            # If duplicate, skip it by not updating tail
+            curr = curr.next
+
+        # Terminate the list
+        tail.next = None
+
+        return dummy.next
