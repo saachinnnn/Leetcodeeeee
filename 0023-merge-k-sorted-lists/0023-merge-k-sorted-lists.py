4a33abb26d967f38acb1080cb127b0f312f12ae9
Sachin <saachinnnn7@gmail.com>
2025-08-04 21:49:51 +0530

    0023-MERGE-K-SORTED-LISTS Stats: Time: 7 ms (89.61%), Space: 20.5 MB (9.76%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -1,21 +1,23 @@
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        vals = []
+        ''' To solve these type of problems.....
 
-        # Step 1: Flatten all node values into a single list
+        1) Simply get the values from the nodes and store it as a list.
+        2) Sort it and the stich it as a list.
+         Done.'''
+
+        vals : list = []
+
         for node in lists:
             while node:
                 vals.append(node.val)
                 node = node.next
-
-        # Step 2: Sort the values
+        
         vals.sort()
 
-        # Step 3: Build new sorted linked list
         dummy = ListNode(-1)
         tail = dummy
         for val in vals:
             tail.next = ListNode(val)
             tail = tail.next
-
         return dummy.next
