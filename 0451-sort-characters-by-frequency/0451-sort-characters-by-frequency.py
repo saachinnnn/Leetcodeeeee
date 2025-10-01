448e1ca52373a0b65e97378be6878c79ef17805f
Sachin <saachinnnn7@gmail.com>
2025-10-01 17:19:49 +0530

    0451-SORT-CHARACTERS-BY-FREQUENCY Stats: Time: 7 ms (77.81%), Space: 19.1 MB (32.58%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
@@ -1,13 +1,8 @@
 from collections import Counter
 class Solution:
     def frequencySort(self, s: str) -> str:
-        # Complete Brute Force.
-        FrequencyMap : dict = Counter(s)
-        ans_str : list = []
-        
-        # Sorting the dictionary.
-        SortedDictionary : list = sorted(FrequencyMap.items(),key = lambda x : x[1], reverse = True)
-        for element,value in SortedDictionary:
-            ans_str.append(element*value)
-        return "".join(ans_str)
+        # The Pythonic approach.
+        freq = Counter(s)
+        sortedfreq = sorted(freq.items(),key = lambda x : x[1],reverse = True)
+        return "".join([element * count for element,count in sortedfreq])
         
