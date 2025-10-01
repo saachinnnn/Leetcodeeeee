88832a5f68230243efae6450e584c4783ba2e766
Sachin <saachinnnn7@gmail.com>
2025-10-01 08:54:39 +0530

    1693-SUM-OF-ALL-ODD-LENGTH-SUBARRAYS Stats: Time: 0 ms (100%), Space: 18 MB (7.96%) - LeetHub

--- 1693-sum-of-all-odd-length-subarrays/1693-sum-of-all-odd-length-subarrays.py
+++ 1693-sum-of-all-odd-length-subarrays/1693-sum-of-all-odd-length-subarrays.py
@@ -1,31 +1,22 @@
 class Solution:
     def sumOddLengthSubarrays(self, arr: List[int]) -> int:
-        # Since its all positive numbers.
+        # Solving it using the maths version...
+
+        # There exists a way to calculate the number of subarrays that are possible.
 
         # Edge cases.
-        if len(arr) == 0:
+        if len(arr) < 0:
             return 0
         
-        ResultSum : int = sum(arr) # The final answer.
+        ResultSum : int = 0
 
-        oddidx : int = 3
-        if oddidx > len(arr):
-            return ResultSum
-        
-        # Now getting the PrefixSum array.
-        PrefixSum : list = [0]*len(arr)
-        PrefixSum[0] = arr[0]
-
-        for i in range(1,len(arr)):
-            PrefixSum[i] = PrefixSum[i - 1] + arr[i]
-        
-        while oddidx:
-            for i in range(len(PrefixSum)):
-                if (i + oddidx - 1) < len(arr):
-                    ResultSum += PrefixSum[i + oddidx - 1] - (PrefixSum[i - 1] if i > 0 else 0)
-                else:
-                    break
-            oddidx += 2
-            if oddidx > len(PrefixSum):
-                oddidx = 0
+        for i in range(len(arr)):
+            TotalSubarrays : int = (i + 1) * (len(arr) - i)
+            OddSubarrays : int = TotalSubarrays // 2
+            if TotalSubarrays % 2 == 1:
+                OddSubarrays += 1
+            ResultSum += OddSubarrays * arr[i]
         return ResultSum
+
+        ''' The formula for calculating the number of subarrays that are possible is (i + 1) * (n(length of the array) * i)
+            This is crucial to solve this problem '''
