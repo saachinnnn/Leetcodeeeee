3a089ac19d9e812dea7251fd0b081326f8631eab
Sachin <saachinnnn7@gmail.com>
2025-10-01 08:57:56 +0530

    1693-SUM-OF-ALL-ODD-LENGTH-SUBARRAYS Stats: Time: 1 ms (77.85%), Space: 17.7 MB (66.25%) - LeetHub

--- 1693-sum-of-all-odd-length-subarrays/1693-sum-of-all-odd-length-subarrays.py
+++ 1693-sum-of-all-odd-length-subarrays/1693-sum-of-all-odd-length-subarrays.py
@@ -1,22 +1,12 @@
 class Solution:
     def sumOddLengthSubarrays(self, arr: List[int]) -> int:
-        # Solving it using the maths version...
-
-        # There exists a way to calculate the number of subarrays that are possible.
-
-        # Edge cases.
-        if len(arr) < 0:
-            return 0
-        
-        ResultSum : int = 0
-
-        for i in range(len(arr)):
-            TotalSubarrays : int = (i + 1) * (len(arr) - i)
-            OddSubarrays : int = TotalSubarrays // 2
-            if TotalSubarrays % 2 == 1:
-                OddSubarrays += 1
-            ResultSum += OddSubarrays * arr[i]
-        return ResultSum
-
-        ''' The formula for calculating the number of subarrays that are possible is (i + 1) * (n(length of the array) * i)
-            This is crucial to solve this problem '''
+        # Solving in Linear time using the Mathematical Approach.
+        n = len(arr)
+        result = 0
+        for i in range(n):
+            total = (i + 1) * (n - i)
+            odd_count = total // 2 + (total % 2)
+            result += arr[i] * odd_count
+        return result
+        # The number of subarrays that include arr[i] is (i + 1) * (n - i)
+        # We count how many of those have odd lengths and multiply by arr[i]
