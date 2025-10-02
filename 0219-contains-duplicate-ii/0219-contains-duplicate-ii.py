dd3bfb5477e500022e80573d4b4f9dd55898db3c
Sachin <saachinnnn7@gmail.com>
2025-10-02 12:49:33 +0530

    0219-CONTAINS-DUPLICATE-II Stats: Time: 35 ms (38.22%), Space: 30.8 MB (71.33%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -1,9 +1,18 @@
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-        # A bit better code would be.
-        DuplicateChecker : dict = {}
-        for idx , num in enumerate(nums):
-            if num in DuplicateChecker and abs(idx - DuplicateChecker[num]) <= k:
+        # There exists a set approach to this as well.
+        
+        ## Defining a set approach with sliding window algorithm.
+
+        windowset : set = set()
+
+        for idx in range(len(nums)):
+
+            if idx > k:
+                windowset.remove(nums[idx - k - 1])
+            
+            if nums[idx] in windowset:
                 return True
-            DuplicateChecker[num] = idx
+            
+            windowset.add(nums[idx])
         return False
