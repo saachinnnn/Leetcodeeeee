e7adb633dc10ed72177cbddbd6ff7f525cb39062
Sachin <saachinnnn7@gmail.com>
2025-10-02 12:45:30 +0530

    0219-CONTAINS-DUPLICATE-II Stats: Time: 27 ms (80.3%), Space: 36.5 MB (58%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -1,19 +1,9 @@
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-        # So I am going to implement a hashmap that stores the elements and their indices.
-        
-        # Edge case.
-        if len(nums) < 2:
-            return False
-        
+        # A bit better code would be.
         DuplicateChecker : dict = {}
-
-        for idx in range(len(nums)): # We traverse through the array
-            if nums[idx] in DuplicateChecker:
-                if abs(idx - DuplicateChecker[nums[idx]]) <= k:
-                    return True
-                else:
-                    DuplicateChecker[nums[idx]] = idx
-            else:
-                DuplicateChecker[nums[idx]] = idx
+        for idx , num in enumerate(nums):
+            if num in DuplicateChecker and abs(idx - DuplicateChecker[num]) <= k:
+                return True
+            DuplicateChecker[num] = idx
         return False
