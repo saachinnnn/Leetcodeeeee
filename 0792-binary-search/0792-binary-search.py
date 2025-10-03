72593e9d908ae5dd5ead54bda996322be7916c23
Sachin <saachinnnn7@gmail.com>
2025-10-03 12:43:58 +0530

    0792-BINARY-SEARCH Stats: Time: 0 ms (100%), Space: 18.9 MB (27.73%) - LeetHub

--- 0792-binary-search/0792-binary-search.py
+++ 0792-binary-search/0792-binary-search.py
@@ -1,20 +1,20 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        if len(nums) is 0:
+        # Now lets type the solution that i have written on my notebook.
+        if len(nums) <= 0:
             return -1
-        elif len(nums) is 1:
-            if nums[0] == target:
-                return 0
-            else:
-                return -1
-        low : int = 0
-        high : int = len(nums) - 1
+        
+        low , high = 0 , len(nums) - 1
+
         while low <= high:
-            mid = (high + low) // 2
+            mid = low + (high - low) // 2
+
             if nums[mid] == target:
-                return mid
+                return mid 
+
             elif nums[mid] > target:
                 high = mid - 1
+            
             else:
                 low = mid + 1
         return -1
