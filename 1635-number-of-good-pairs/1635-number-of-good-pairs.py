6bfa5b4b95babed818dbceb078fc83b7c318cd15
Sachin <saachinnnn7@gmail.com>
2025-10-07 20:31:56 +0530

    1635-NUMBER-OF-GOOD-PAIRS Stats: Time: 4 ms (6.3%), Space: 17.9 MB (9.53%) - LeetHub

--- 1635-number-of-good-pairs/1635-number-of-good-pairs.py
+++ 1635-number-of-good-pairs/1635-number-of-good-pairs.py
@@ -1,10 +1,7 @@
 class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
         # I will brute force this and end it right it here.
-        ## Nah sike i am going to use a dictionary.
+        ## Nah sike i am going to use a dictionary
+        return sum(nums[right + 1:].count(nums[right]) if nums[right] in nums[right + 1:] else 0 for right in range(len(nums)))
 
-        ans : int = 0
-        for right in range(len(nums)):
-            if nums[right] in nums[right + 1:]:
-                ans += nums[right + 1:].count(nums[right])
-        return ans
+        # Damnnnn the above solution is cool af.
