11976b072f06232d8121569387e6e9997926be19
Sachin <saachinnnn7@gmail.com>
2025-10-13 20:09:25 +0530

    1353-FIND-RESULTANT-ARRAY-AFTER-REMOVING-ANAGRAMS Stats: Time: 31 ms (10.2%), Space: 17.8 MB (47.41%) - LeetHub

--- 1353-find-resultant-array-after-removing-anagrams/1353-find-resultant-array-after-removing-anagrams.py
+++ 1353-find-resultant-array-after-removing-anagrams/1353-find-resultant-array-after-removing-anagrams.py
@@ -1,10 +1,11 @@
 from collections import Counter
+from typing import List
+
 class Solution:
     def removeAnagrams(self, words: List[str]) -> List[str]:
-        idx : int = 1
-        while idx < len(words):
-            if Counter(words[idx]) == Counter(words[idx - 1]):
-                words.pop(idx)
-            else:
-                idx += 1
+        result = [words[0]]
+        for word in words[1:]:
+            if Counter(word) != Counter(result[-1]):
+                result.append(word)
+        words = result
         return words
