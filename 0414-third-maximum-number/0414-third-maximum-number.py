a1964a77053e0b7dd95d6357cf5ebf432093d0e1
Sachin <saachinnnn7@gmail.com>
2025-10-20 22:41:29 +0530

    0414-THIRD-MAXIMUM-NUMBER Stats: Time: 0 ms (100%), Space: 19.1 MB (49.8%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.py
+++ 0414-third-maximum-number/0414-third-maximum-number.py
@@ -1,13 +1,9 @@
 class Solution:
     def thirdMax(self, nums: List[int]) -> int:
-        nums = list(set(nums))
-
-        if len(nums) <= 2:
+        nums = set(nums)
+        if len(nums) < 3:
             return max(nums)
-        
-        for iteration in range(2):
-            value = max(nums)
-            index = nums.index(value)
-            # Now.
-            nums[index] = -10000000000000000
-        return max(nums)
+        else:
+            nums.remove(max(nums))
+            nums.remove(max(nums))
+            return max(nums)
