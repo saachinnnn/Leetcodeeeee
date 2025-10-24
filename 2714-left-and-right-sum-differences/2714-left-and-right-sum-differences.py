36acb6c6b836b5f995e35e8e931151f503583bbd
Sachin <saachinnnn7@gmail.com>
2025-10-24 22:46:29 +0530

    2714-LEFT-AND-RIGHT-SUM-DIFFERENCES Stats: Time: 4 ms (45.67%), Space: 18 MB (53.77%) - LeetHub

--- 2714-left-and-right-sum-differences/2714-left-and-right-sum-differences.py
+++ 2714-left-and-right-sum-differences/2714-left-and-right-sum-differences.py
@@ -1,14 +1,12 @@
 class Solution:
     def leftRightDifference(self, nums: List[int]) -> List[int]:
-        # Brute force would be to compute two arrays where we get prefix and suffixsums.
-        leftsum : list = [0]*len(nums)
-        for idx in range(len(nums) - 1):
-            leftsum[idx + 1]  = leftsum[idx] + nums[idx]
-        rightsum : list = [0]*len(nums)
-        for idx in range(len(nums) - 1,0,-1):
-            rightsum[idx - 1] = rightsum[idx] + nums[idx]
-
-        # Now that we have computed both arrays which is memory expensive but O(2N) ~ O(N)
-        # Thus.
-        return [abs(leftsum[idx] - rightsum[idx]) for idx in range(len(nums))]
-
+        # One of the most clever solutions would be....
+        leftsum : int = 0
+        rightsum : int = sum(nums) # Precomputing the values.
+        
+        answer : list = []
+        for idx in range(len(nums)):
+            answer.append(abs(leftsum - (rightsum - nums[idx])))
+            leftsum += nums[idx]
+            rightsum -= nums[idx]
+        return answer
