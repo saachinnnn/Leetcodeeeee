ad6e6e309d48588eef9edd0ff1518dc6dae38593
Sachin <saachinnnn7@gmail.com>
2025-10-28 12:25:43 +0530

    0496-NEXT-GREATER-ELEMENT-I Stats: Time: 20 ms (20.07%), Space: 17.9 MB (64.6%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,33 +1,31 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-       # Let me just start to solve this question in brute force method.
-       ## Nah actually using a stack is much more easier.
+        # Using the optimal approach.
+
+        ## Using a Next Greater Element Stack.
 
         if not nums1 or not nums2:
-            return []
+            return [] # Weird ahh edge case.
         
-        stack = []
-        # Precomputing the next greater element stack.
-        nge = []
+        nge_stk : list = [0]*len(nums2)
+        stk : list = []
         for idx in range(len(nums2) - 1,-1,-1):
             if idx == len(nums2) - 1:
-                nge.append(-1)
-                stack.append(nums2[idx]) # We append the element itself.
+                nge_stk[idx] = -1
+                stk.append(nums2[idx])
             else:
-                while stack and nums2[idx] > stack[-1]:
-                    stack.pop()
-                if stack == []:
-                    nge.append(-1)
-                    stack.append(nums2[idx])
+                while stk and nums2[idx] > stk[-1]:
+                    stk.pop()
+                if stk == []:
+                    nge_stk[idx] = -1
+                    stk.append(nums2[idx])
                 else:
-                    nge.append(stack[-1])
-                    stack.append(nums2[idx])
-        
-        # Now.
-        nge = nge[::-1]
+                    nge_stk[idx] = stk[-1]
+                    stk.append(nums2[idx])
+        # So we have computed the next greater stack above successfully!! sui!!!.
         ans = []
         for element in nums1:
             index = nums2.index(element)
-            ans.append(nge[index])
+            ans.append(nge_stk[index])
         return ans
-       
+
