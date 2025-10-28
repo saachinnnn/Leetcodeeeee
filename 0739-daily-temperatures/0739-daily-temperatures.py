187d30ced9d4054203522ba4306b3412b4cb521d
Sachin <saachinnnn7@gmail.com>
2025-10-28 16:24:58 +0530

    0739-DAILY-TEMPERATURES Stats: Time: 108 ms (54.44%), Space: 26.8 MB (90.99%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,27 +1,21 @@
+from typing import List
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        # Okay I am going to solve this.
-        if not temperatures:
-            return []
-        
-        # I think this approach 
-        ans : list = [0]*len(temperatures)
+        # Instead of appending the distance to the stack as well which makes things more complicated, we can calculate the distance directly.
+        ## So...
+
+        ans : List[int] = [0]*len(temperatures)
         stack : list = []
         for idx in range(len(temperatures) - 1,-1,-1):
-            temperature = 1
-            if idx == len(temperatures) - 1:
-                ans[idx] = 0
-                stack.append((temperatures[idx],1))
-            else:
-                while stack and temperatures[idx] >= stack[-1][0]:
-                    temp = stack.pop()
-                    temperature += temp[1]
-                if stack == []:
-                    ans[idx] = 0
-                else:
-                    ans[idx] = temperature
-                stack.append((temperatures[idx],temperature))
+            while stack and temperatures[idx] >= temperatures[stack[-1]]:
+                stack.pop()
+            if stack:
+                ans[idx] = stack[-1] - idx
+            stack.append(idx)
         return ans
-            
-            
-                    
+# In the above approach we compute the distance simply by saving the index of the element in stack and subtracting it with current index traversing through the loop so that we can get the distance for the answer.
+## Thus that distance is updated with the answer array.
+### Much simpler solution than what i did earlier but both works in the same time complexity.
+
+
+# So its not essentialy to add the distance/span to the stack like we did with stock and span question earlier.
