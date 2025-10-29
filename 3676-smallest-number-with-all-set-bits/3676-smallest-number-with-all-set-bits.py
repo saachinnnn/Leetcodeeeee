2892350dba375ffdb29dfa4e164b22d6baeab842
Sachin <saachinnnn7@gmail.com>
2025-10-29 15:50:23 +0530

    3676-SMALLEST-NUMBER-WITH-ALL-SET-BITS Stats: Time: 0 ms (100%), Space: 17.7 MB (75.09%) - LeetHub

--- 3676-smallest-number-with-all-set-bits/3676-smallest-number-with-all-set-bits.py
+++ 3676-smallest-number-with-all-set-bits/3676-smallest-number-with-all-set-bits.py
@@ -1,6 +1,23 @@
 class Solution:
     def smallestNumber(self, n: int) -> int:
-        while n:
-            if '0' not in bin(n)[2:]:
-                return n
-            n += 1
+        outputs = {
+            1: 1,
+            2: 3,
+            4: 7,
+            8: 15,
+            16: 31,
+            32: 63,
+            64: 127,
+            128: 255,
+            256: 511,
+            512: 1023,
+            1024: 2047,
+        }
+        prev = 1
+        for output in outputs.keys():
+            if output > n:
+                return outputs[prev]
+            prev = output
+        
+        return outputs[1024]
+
