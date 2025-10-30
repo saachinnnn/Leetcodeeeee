5ff49acb478d09ba4dff17d1b5293d8d1f527471
Sachin <saachinnnn7@gmail.com>
2025-10-30 10:25:24 +0530

    0503-NEXT-GREATER-ELEMENT-II Stats: Time: 31 ms (32.86%), Space: 19.3 MB (77.64%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.py
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.py
@@ -2,17 +2,16 @@ class Solution:
     def nextGreaterElements(self, nums: List[int]) -> List[int]:
         # What if I just scale the array into twice the size.
         n = len(nums)
-        nums = nums + nums
         stack : list = []
-        ansarray : list = [0]*len(nums)
-        for idx in range(len(nums) - 1,-1,-1):
-            while stack and nums[idx] >= stack[-1]:
+        ansarray : list = [0]*n
+        for idx in range((2*n) - 1,-1,-1):
+            while stack and nums[idx%n] >= stack[-1]:
                 stack.pop()
             if not stack:
-                ansarray[idx] = -1
+                ansarray[idx%n] = -1
             else:
-                ansarray[idx] = stack[-1]
-            stack.append(nums[idx])
+                ansarray[idx%n] = stack[-1]
+            stack.append(nums[idx%n])
         return ansarray[:n]
 
         
