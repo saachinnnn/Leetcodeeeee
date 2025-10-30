ebfbc7eec1f0eae2149f727106753e1385a5f568
Sachin <saachinnnn7@gmail.com>
2025-10-30 09:32:34 +0530

    0084-LARGEST-RECTANGLE-IN-HISTOGRAM Stats: Time: 159 ms (54.76%), Space: 29.7 MB (76.19%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
@@ -1,16 +1,19 @@
-from typing import List
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
-        # Monotonic stack solution.
+        # So i will solve this one last time.
+        ## Basically we use a monotonic stack here to check if the previous element is greater.
+        # So....
         if not heights:
             return 0
+        
         maxarea : int = 0
-        stack : List[int] = []
-        for idx in range(len(heights) + 1):
-            curr_height = heights[idx] if idx < len(heights) else 0
+        stack : list = []
+        for idx in range(len(heights) + 1): # This is to compute the value of the last rectangle as well.
+            curr_height : int = heights[idx] if idx < len(heights) else 0 # For the last case.
             while stack and curr_height < heights[stack[-1]]:
-                height = heights[stack.pop()]
-                width = idx if not stack else idx - stack[-1] - 1
-                maxarea = max(height*width,maxarea)
+                # Calculate the height and width of the previous rectangles then.
+                height : int = heights[stack.pop()]
+                width : int = idx if not stack else idx - stack[-1] - 1
+                maxarea = max(maxarea,width * height)
             stack.append(idx)
         return maxarea
