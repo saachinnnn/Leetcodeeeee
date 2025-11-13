eae4409b9a5402dcba7886d06aceb83ea21f85f3
Sachin <saachinnnn7@gmail.com>
2025-11-13 22:01:34 +0530

    1797-GOAL-PARSER-INTERPRETATION Stats: Time: 36 ms (69.73%), Space: 17.6 MB (65.56%) - LeetHub

--- 1797-goal-parser-interpretation/1797-goal-parser-interpretation.py
+++ 1797-goal-parser-interpretation/1797-goal-parser-interpretation.py
@@ -1,11 +1,3 @@
 class Solution:
     def interpret(self, command: str) -> str:
-        # Using stack.
-        stk = []
-        for idx in range(len(command)):
-            if command[idx] == '(':
-                if command[idx + 1] == ')':
-                    stk.append('o')
-            elif command[idx].isalpha():
-                stk.append(command[idx])
-        return ''.join(stk)
+        return command.replace('()','o').replace('(al)','al')
