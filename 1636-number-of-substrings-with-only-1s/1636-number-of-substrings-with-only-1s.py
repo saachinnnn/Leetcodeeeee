10baefd4de48115858238f008ce7a37f70042993
Sachin <saachinnnn7@gmail.com>
2025-11-16 13:13:11 +0530

    1636-NUMBER-OF-SUBSTRINGS-WITH-ONLY-1S Stats: Time: 44 ms (5.62%), Space: 18.3 MB (22.1%) - LeetHub

--- 1636-number-of-substrings-with-only-1s/1636-number-of-substrings-with-only-1s.py
+++ 1636-number-of-substrings-with-only-1s/1636-number-of-substrings-with-only-1s.py
@@ -1,6 +1,7 @@
 class Solution:
     def numSub(self, s: str) -> int:
         # Apparently this is solvable in one line.
+        Mod = 10**9 + 7
         Total = 0
         ones = 1
         for idx in range(len(s)):
@@ -11,4 +12,4 @@ class Solution:
                 else:
                     Total += 1
                     ones = 1
-        return Total % (10**9 + 7)
+        return Total % Mod
