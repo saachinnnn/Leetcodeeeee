629204d88e894ec0256bde0cdafd2884d9e5bf50
Sachin <saachinnnn7@gmail.com>
2025-11-16 12:58:18 +0530

    1636-NUMBER-OF-SUBSTRINGS-WITH-ONLY-1S Stats: Time: 47 ms (5.62%), Space: 17.9 MB (97.75%) - LeetHub

--- 1636-number-of-substrings-with-only-1s/1636-number-of-substrings-with-only-1s.py
+++ 1636-number-of-substrings-with-only-1s/1636-number-of-substrings-with-only-1s.py
@@ -1,13 +1,14 @@
 class Solution:
     def numSub(self, s: str) -> int:
-        ones = 0
+        # Apparently this is solvable in one line.
         Total = 0
-        for ch in s:
-            if ch == '1':
-                ones += 1
-            else:
-                Total += (ones * (ones + 1)) // 2
-                ones = 0
-        # Final streak of '1's (if any)
-        Total += (ones * (ones + 1)) // 2
+        ones = 1
+        for idx in range(len(s)):
+            if s[idx] == '1':
+                if idx < len(s) - 1 and s[idx + 1] == '1':
+                    Total += ones + 1
+                    ones += 1
+                else:
+                    Total += 1
+                    ones = 1
         return Total % (10**9 + 7)
